9dd104c704be59dfd2881f97555d284f2cf21df8
Angela Chua <amtchua@myseneca.ca>
2025-04-03 04:56:06 -0400

    Added favorites functionality

---
+++ poketrade/src/pages/api/user/favorite.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+import User from "@/data/User";
+
+export default async function handler(req, res) {
+  if (!(req.method === "POST" || req.method === "DELETE")) {
+    return res.status(405).end("Not supported");
+  }
+  const { username } = req.body;
+  const { card } = req.query;
+  const db = process.env.DB_URI;
+
+  await mongoose
+    .connect(db)
+    .then(async () => {
+      console.log("MongoDB connected");
+      User.findOne({ username: username })
+        .then((u) => {
+          if (!u) {
+            mongoose.connection.close();
+            return res.status(404).end("User doesn't exist");
+          }
+          const cardObj = u.favorites.find((c) => c.cardId === card);
+          switch (req.method) {
+            case "POST":
+              if (!cardObj) u.favorites.push({ cardId: card });
+              else
+                return res
+                  .status(400)
+                  .end("This card already exists in this user's Favorites");
+              break;
+            case "DELETE":
+              if (cardObj)
+                u.favorites.splice(u.favorites.indexOf({ cardId: card }), 1);
+              else
+                return res
+                  .status(400)
+                  .end("This card doesn't exist in this user's Favorites");
+              break;
+          }
+          User.updateOne(
+            { username: username },
+            { $set: { favorites: u.favorites } }
+          )
+            .then(() => {
+              mongoose.connection.close();
+              return res
+                .status(200)
+                .end(
+                  `${req.method === "POST" ? "Added" : "Deleted"} card ` +
+                    card +
+                    ` ${req.method === "POST" ? "to" : "from"} ` +
+                    username +
+                    `'s favorites`
+                );
+            })
+            .catch((err) => {
+              mongoose.connection.close();
+              console.error(err);
+              return res.status(500).end("An error has occurred");
+            });
+        })
+        .catch((err) => {
+          mongoose.connection.close();
+          console.error(err);
+          return res.status(500).end("An error has occurred");
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      mongoose.connection.close();
+      return res.status(500).end("An error has occurred");
+    });
+}
