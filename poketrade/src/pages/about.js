bbb7d40e9bc8a19cdd3fbd966bc7a1b1dc0ec08b
Angela Chua <amtchua@myseneca.ca>
2025-04-09 09:54:49 -0400

    Edited About page

--- poketrade/src/pages/about.js
+++ poketrade/src/pages/about.js
@@ -1,8 +1,33 @@
+"use client";
+import { Card, Col, Container, Row } from "react-bootstrap";
+
 export default function AboutPage() {
-    return (
-      <div className="container mt-4">
-        <h1>About</h1>
-        <p>This is a basic About page.</p>
-      </div>
-    );
-  }
+  return (
+    <>
+      <Card
+        className={"mx-auto "}
+        style={{ maxWidth: "540px", marginTop: "15%" }}
+      >
+        <Row className={"g-0"}>
+          <Col className={"md-4"}>
+            <Card.Img
+              src="..."
+              className={"img-fluid rounded-start"}
+              alt="..."
+            />
+          </Col>
+          <Col className={"md-8"}>
+            <Card.Body>
+              <Card.Title>About Us</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
+    </>
+  );
+}
