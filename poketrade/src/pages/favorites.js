0292724c654499106bde32a05afa7ec86b5f652c
Angela Chua <amtchua@myseneca.ca>
2025-04-09 15:00:04 -0400

    Reworked favorites page, added pagination

--- poketrade/src/pages/favorites.js
+++ poketrade/src/pages/favorites.js
@@ -7,18 +7,37 @@ import Link from "next/link";
 import { set } from "mongoose";
 import CardAPIItem from "components/CardAPIItem";
 import CardList from "components/CardList";
+import Pagination from "components/Pagination";
 
 export default function FavoritesPage() {
   const router = useRouter();
   const [loggedIn] = useAtom(loggedInAtom);
   const [favList, setFavList] = useState([]);
   const [eleList, setEleList] = useState(<>Loading...</>);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentCards, setCurrentCards] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
+  const [startIndex, setStartIndex] = useState(0);
+  const cardsPerPage = 4;
+
+  const handlePageChange = (page) => {
+    if (page >= 1 && page <= totalPages) {
+      setCurrentPage(page);
+      setStartIndex((page - 1) * cardsPerPage);
+    }
+  };
+
   useEffect(() => {
+    let len = 0;
+    setEleList(<>Loading...</>);
     if (!loggedIn && !localStorage.getItem("token")) {
       router.push("/login");
       return;
     }
     setFavList([]);
+    setCurrentCards([]);
+
     const token = localStorage.getItem("token");
     fetch("/api/user/get/favorites", {
       method: "GET",
@@ -28,7 +47,13 @@ export default function FavoritesPage() {
         return res.json();
       })
       .then((val) => {
-        for (let i = 0; i < val.favorites.length && i < 50; i++) {
+        console.log(startIndex);
+        len = Math.ceil(val.favorites.length / cardsPerPage);
+        for (
+          let i = startIndex, count = 0;
+          i < val.favorites.length && count < cardsPerPage;
+          i++, count++
+        ) {
           fetch(`/api/products/${val.favorites[i].cardId}`)
             .then((res) => {
               return res.json();
@@ -42,17 +67,23 @@ export default function FavoritesPage() {
       })
       .catch((err) => console.log("Favorites error" + err));
 
-    // loads page after 3 seconds to account for loading data via the api
+    // loads page after 500 milliseconds to account for loading data via the api
     setTimeout(() => {
-      console.log(favList);
-      setEleList(<>{<CardList cards={favList} useLocal={true} />}</>);
-    }, 3000);
-  }, [router.isReady]);
+      if (currentCards.length === 0) currentCards.push(...favList);
+      setTotalPages(len);
+      setEleList(<>{<CardList cards={currentCards} useLocal={true} />}</>);
+    }, 500);
+  }, [router.isReady, currentPage]);
   return (
     <>
       <div className="container mt-4">
         <h1>Favorites</h1>
         {eleList}
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+        />
       </div>
     </>
   );
