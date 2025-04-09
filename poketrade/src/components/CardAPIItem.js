142f15d73474f66ac3768e9aca998b7d730a8e80
Angela Chua <amtchua@myseneca.ca>
2025-04-09 01:15:51 -0400

    Added CardItem but for cards taken directly from PokemonTCGAPI

---
+++ poketrade/src/components/CardAPIItem.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Container, Row, Col, Card } from "react-bootstrap";
+
+export default function CardAPIItem(props) {
+  const p = props.card.data;
+  const hasButton = props.hasButton;
+  console.log(p);
+  return (
+    <>
+      <Card className="h-100">
+        <Card.Img variant="top" src={p.images?.small} alt={p.name} />
+        <Card.Body>
+          <Card.Title>{p.name}</Card.Title>
+          <Card.Text>{p.flavorText}</Card.Text>
+          <Card.Text>
+            <strong>
+              ${(p.cardmarket.prices.averageSellPrice / 100).toFixed(2)}
+            </strong>
+          </Card.Text>
+        </Card.Body>
+        {hasButton && (
+          <Link href={`/cards/${p.id}`} className="btn btn-primary">
+            View Card
+          </Link>
+        )}
+      </Card>
+    </>
+  );
+}
