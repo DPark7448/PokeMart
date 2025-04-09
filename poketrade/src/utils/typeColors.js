0f2a9cc7258429537bb967847474d77b59c17a53
aomar22 <aomar22@myseneca.ca>
2025-04-09 07:52:39 -0400

    fixing deployment error by adding utils/typeColors.js

---
+++ poketrade/src/utils/typeColors.js
@@ -0,0 +1,11 @@
+const typeColors = {
+    fire: 'danger',
+    water: 'primary',
+    grass: 'success',
+    electric: 'warning',
+    flying: 'info',
+    normal: 'secondary',
+    // Add more categories as needed.
+  };
+  
+  export default typeColors;
