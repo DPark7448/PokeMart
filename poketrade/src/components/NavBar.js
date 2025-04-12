1e5dee853be22e5d90a5e38cc98a803ee5487de0
GranDog123 <113808750+GranDog123@users.noreply.github.com>
2025-04-12 19:20:46 -0400

    Update NavBar.js

--- poketrade/src/components/NavBar.js
+++ poketrade/src/components/NavBar.js
@@ -55,7 +55,7 @@ export default function NavBar() {
       <div className="container-fluid d-flex align-items-center justify-content-between position-relative">
         <div className="d-flex align-items-center gap-4 fs-5">
           <Link href="/" className="navbar-brand fw-bold fs-4 mb-0 Text-dark">
-            Poke Mart
+            Poké Mart
           </Link>
           <Link href="/cards" className="nav-link px-2">
             Card List
