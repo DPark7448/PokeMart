6612edb22c5715e940fbaed3585c1661b23961ad
Angela Chua <amtchua@myseneca.ca>
2025-04-15 21:13:37 -0400

    revert back to the way it was before

--- poketrade/src/pages/_app.js
+++ poketrade/src/pages/_app.js
@@ -1,19 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect } from "react";
-import NavBar from "../components/NavBar";
 import { useRouter } from "next/router"; //to redirect
 import { useAtom } from "jotai";
 import { loggedInAtom } from "../store/loginAtom";
-import { favoritesAtom } from "../store/favoritesAtom";
-import { searchHistoryAtom } from "../store/searchHistoryAtom";
 import Layout from "../components/Layout";
 import RouteGuard from "../components/RouteGuard";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const [loggedIn, setLoggedIn] = useAtom(loggedInAtom);
-  const [favorites, setFavorites] = useAtom(favoritesAtom);
-  const [searchHistory, setSearchHistory] = useAtom(searchHistoryAtom);
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
@@ -22,40 +17,6 @@ function MyApp({ Component, pageProps }) {
     if (!localStorage.getItem("token")) {
       setLoggedIn(false);
     } else setLoggedIn(true);
-  }, [router]);
-
-  useEffect(() => {
-    async function fetchDataOne() {
-      const favList = localStorage.getItem("favorites");
-      const token = localStorage.getItem("token");
-      if (!favList) {
-        localStorage.setItem("favorites", "[]");
-      } else if (token) {
-        const data = await fetch("/api/user/get/favorites", {
-          headers: { authorization: token },
-        }).then((res) => res.json());
-        setFavorites(data);
-        localStorage.setItem("favorites", JSON.stringify(data));
-      }
-    }
-    fetchDataOne();
-  }, [router]);
-
-  useEffect(() => {
-    async function fetchDataTwo() {
-      const hisList = localStorage.getItem("history");
-      const token = localStorage.getItem("token");
-      if (!hisList) {
-        localStorage.setItem("history", "[]");
-      } else if (token) {
-        const data = await fetch("/api/user/get/history", {
-          headers: { authorization: token },
-        }).then((res) => res.json());
-        setSearchHistory(data);
-        localStorage.setItem("history", JSON.stringify(data));
-      }
-    }
-    fetchDataTwo();
   }, [router]);
 
   return (
